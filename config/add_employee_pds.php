ae54a68be9cbd78249b522d95428342ca6d1d194
Zyprush <alberiojake27@gmail.com>
2023-10-24 16:04:10 +0800

    nonsense

--- config/add_employee_pds.php
+++ config/add_employee_pds.php
@@ -55,6 +55,57 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $spouse_business_address = $_POST['spouse_business_address'];
     $spouse_telephone = $_POST['spouse_telephone'];
 
+    //parents
+    $father_surname = $_POST['father_surname'];
+    $father_firstname = $_POST['father_firstname'];
+    $father_middlename = $_POST['father_middlename'];
+    $father_extension = $_POST['father_extension'];
+    $mother_surname = $_POST['mother_surname'];
+    $mother_firstname = $_POST['mother_firstname'];
+    $mother_middlename = $_POST['mother_middlename'];
+
+    //Schools
+    $elem_nameOfSchool = $_POST['elem_nameOfSchool'];
+    $elem_degree = $_POST['elem_degree'];
+    $elem_from = $_POST['elem_from'];
+    $elem_to = $_POST['elem_to'];
+    $elem_high = $_POST['elem_high'];
+    $elem_grad = $_POST['elem_grad'];
+    $elem_honor = $_POST['elem_honor'];
+
+    $sec_nameOfSchool = $_POST['sec_nameOfSchool'];
+    $sec_degree = $_POST['sec_degree'];
+    $sec_from = $_POST['sec_from'];
+    $sec_to = $_POST['sec_to'];
+    $sec_high = $_POST['sec_high'];
+    $sec_grad = $_POST['sec_grad'];
+    $sec_honor = $_POST['sec_honor'];
+
+    $voc_nameOfSchool = $_POST['voc_nameOfSchool'];
+    $voc_degree = $_POST['voc_degree'];
+    $voc_from = $_POST['voc_from'];
+    $voc_to = $_POST['voc_to'];
+    $voc_high = $_POST['voc_high'];
+    $voc_grad = $_POST['voc_grad'];
+    $voc_honor = $_POST['voc_honor'];
+
+    $college_nameOfSchool = $_POST['college_nameOfSchool'];
+    $college_degree = $_POST['college_degree'];
+    $college_from = $_POST['college_from'];
+    $college_to = $_POST['college_to'];
+    $college_high = $_POST['college_high'];
+    $college_grad = $_POST['college_grad'];
+    $college_honor = $_POST['college_honor'];
+
+    $grad_nameOfSchool = $_POST['grad_nameOfSchool'];
+    $grad_degree = $_POST['grad_degree'];
+    $grad_from = $_POST['grad_from'];
+    $grad_to = $_POST['grad_to'];
+    $grad_high = $_POST['grad_high'];
+    $grad_grad = $_POST['grad_grad'];
+    $grad_honor = $_POST['grad_honor'];
+
+
     // Validate employee details here if needed
 
     // Check if the 'employees' table exists, and create it if not
@@ -70,60 +121,129 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // The table doesn't exist, create it
         $createTableQuery = "CREATE TABLE employees (
             ID INT AUTO_INCREMENT PRIMARY KEY,
-            FirstName VARCHAR(255) NOT NULL,
-            MiddleName VARCHAR(255) NOT NULL,
-            LastName VARCHAR(255) NOT NULL,
-            Extension VARCHAR(255),
+            FirstName TEXT NOT NULL,
+            MiddleName TEXT NOT NULL,
+            LastName TEXT NOT NULL,
+            Extension TEXT,
             StartDate DATE,
-            Type VARCHAR(255) NOT NULL,
-            Department VARCHAR(255) NOT NULL,
-            Position VARCHAR(255) NOT NULL,
+            Type TEXT NOT NULL,
+            Department TEXT,
+            Position TEXT NOT NULL,
             EndDate DATE,
             Birthdate DATE,
-            PlaceOfBirth VARCHAR(255),
-            Sex VARCHAR(10),
-            CivilStatus VARCHAR(20),
+            PlaceOfBirth TEXT,
+            Sex VARCHAR(10),  -- VARCHAR data type is kept for Sex
+            CivilStatus TEXT,
             Height FLOAT,
             Weight FLOAT,
-            BloodType VARCHAR(5),
-            GSIS VARCHAR(20),
-            PagIbig VARCHAR(20),
-            SSS VARCHAR(20),
-            TIN VARCHAR(20),
-            Agency VARCHAR(255),
-            Citizenship VARCHAR(255),
-            DualCitizenship VARCHAR(255),
-            LotNo VARCHAR(255),
-            Street VARCHAR(255),
-            Barangay VARCHAR(255),
-            Subdivision VARCHAR(255),
-            City VARCHAR(255),
-            Province VARCHAR(255),
-            Zipcode VARCHAR(20),
-            LotNoPermanent VARCHAR(255),
-            StreetPermanent VARCHAR(255),
-            BarangayPermanent VARCHAR(255),
-            SubdivisionPermanent VARCHAR(255),
-            CityPermanent VARCHAR(255),
-            ProvincePermanent VARCHAR(255),
-            ZipcodePermanent VARCHAR(20),
-            Telephone VARCHAR(20),
-            Mobile VARCHAR(20),
-            Email VARCHAR(255),
-            SpouseSurname VARCHAR(255),
-            SpouseFirstname VARCHAR(255),
-            SpouseMiddlename VARCHAR(255),
-            SpouseExtensionname VARCHAR(255),
-            SpouseOccupation VARCHAR(255),
-            SpouseEmployer VARCHAR(255),
-            SpouseBusinessAddress VARCHAR(255),
-            SpouseTelephone VARCHAR(20)
+            BloodType TEXT,
+            GSIS TEXT,
+            PagIbig TEXT,
+            SSS TEXT,
+            TIN TEXT,
+            Agency TEXT,
+            Citizenship TEXT,
+            DualCitizenship TEXT,
+            LotNo TEXT,
+            Street TEXT,
+            Barangay TEXT,
+            Subdivision TEXT,
+            City TEXT,
+            Province TEXT,
+            Zipcode TEXT,
+            LotNoPermanent TEXT,
+            StreetPermanent TEXT,
+            BarangayPermanent TEXT,
+            SubdivisionPermanent TEXT,
+            CityPermanent TEXT,
+            ProvincePermanent TEXT,
+            ZipcodePermanent TEXT,
+            Telephone TEXT,
+            Mobile TEXT,
+            Email TEXT,
+            SpouseSurname TEXT,
+            SpouseFirstname TEXT,
+            SpouseMiddlename TEXT,
+            SpouseExtensionname TEXT,
+            SpouseOccupation TEXT,
+            SpouseEmployer TEXT,
+            SpouseBusinessAddress TEXT,
+            SpouseTelephone TEXT,
+            FatherSurname TEXT,
+            FatherFirstname TEXT,
+            FatherMiddlename TEXT,
+            FatherExtension TEXT,
+            MotherSurname TEXT,
+            MotherFirstname TEXT,
+            MotherMiddlename TEXT,
+            ElemNameOfSchool TEXT,
+            ElemDegree TEXT,
+            ElemFrom TEXT,
+            ElemTo TEXT,
+            ElemHigh TEXT,
+            ElemGrad TEXT,
+            ElemHonor TEXT,
+            SecNameOfSchool TEXT,
+            SecDegree TEXT,
+            SecFrom TEXT,
+            SecTo TEXT,
+            SecHigh TEXT,
+            SecGrad TEXT,
+            SecHonor TEXT,
+            VocNameOfSchool TEXT,
+            VocDegree TEXT,
+            VocFrom TEXT,
+            VocTo TEXT,
+            VocHigh TEXT,
+            VocGrad TEXT,
+            VocHonor TEXT,
+            CollegeNameOfSchool TEXT,
+            CollegeDegree TEXT,
+            CollegeFrom TEXT,
+            CollegeTo TEXT,
+            CollegeHigh TEXT,
+            CollegeGrad TEXT,
+            CollegeHonor TEXT,
+            GradNameOfSchool TEXT,
+            GradDegree TEXT,
+            GradFrom TEXT,
+            GradTo TEXT,
+            GradHigh TEXT,
+            GradGrad TEXT,
+            GradHonor TEXT
         )";
-
+    
         if (mysqli_query($conn, $createTableQuery)) {
             echo 'Table "employees" created successfully.';
         } else {
             echo 'Error creating table: ' . mysqli_error($conn);
+            exit;
+        }
+    }
+    
+
+    // Check if the 'children' table exists, and create it if not
+    $checkChildrenTableQuery = "SHOW TABLES LIKE 'children'";
+    $childrenTableResult = mysqli_query($conn, $checkChildrenTableQuery);
+
+    if (!$childrenTableResult) {
+        echo 'Error checking "children" table existence: ' . mysqli_error($conn);
+        exit;
+    }
+
+    if (mysqli_num_rows($childrenTableResult) == 0) {
+        // The 'children' table doesn't exist, create it
+        $createChildrenTableQuery = "CREATE TABLE children (
+            ID INT AUTO_INCREMENT PRIMARY KEY,
+            EmployeeID INT,
+            ChildName VARCHAR(255) NOT NULL,
+            ChildDOB DATE
+        )";
+
+        if (mysqli_query($conn, $createChildrenTableQuery)) {
+            echo 'Table "children" created successfully.';
+        } else {
+            echo 'Error creating "children" table: ' . mysqli_error($conn);
             exit;
         }
     }
@@ -135,14 +255,26 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         Agency, Citizenship, DualCitizenship, LotNo, Street, Barangay, Subdivision, City, Province, Zipcode,
         LotNoPermanent, StreetPermanent, BarangayPermanent, SubdivisionPermanent, CityPermanent, ProvincePermanent,
         ZipcodePermanent, Telephone, Mobile, Email, SpouseSurname, SpouseFirstname, SpouseMiddlename,
-        SpouseExtensionname, SpouseOccupation, SpouseEmployer, SpouseBusinessAddress, SpouseTelephone
+        SpouseExtensionname, SpouseOccupation, SpouseEmployer, SpouseBusinessAddress, SpouseTelephone, FatherSurname, FatherFirstname, FatherMiddlename, FatherExtension,
+        MotherSurname, MotherFirstname, MotherMiddlename,
+        ElemNameOfSchool, ElemDegree, ElemFrom, ElemTo, ElemHigh, ElemGrad, ElemHonor,
+        SecNameOfSchool, SecDegree, SecFrom, SecTo, SecHigh, SecGrad, SecHonor,
+        VocNameOfSchool, VocDegree, VocFrom, VocTo, VocHigh, VocGrad, VocHonor,
+        CollegeNameOfSchool, CollegeDegree, CollegeFrom, CollegeTo, CollegeHigh, CollegeGrad, CollegeHonor,
+        GradNameOfSchool, GradDegree, GradFrom, GradTo, GradHigh, GradGrad, GradHonor
     ) VALUES (
         '$firstName', '$middleName', '$lastName', '$extension', '$startDate', '$type', '$department', '$position', '$endDate',
         '$birthdate', '$placeOfBirth', '$sex', '$civilStatus', $height, $weight, '$bloodtype', '$gsis', '$pagibig', '$sss', '$tin',
         '$agency', '$citizenship', '$dualCitizenship', '$lotNo', '$street', '$barangay', '$subdivision', '$city', '$province', '$zipcode',
         '$lotNo_permanent', '$street_permanent', '$barangay_permanent', '$subdivision_permanent', '$city_permanent', '$province_permanent', '$zipcode_permanent',
         '$telephone', '$mobile', '$email', '$spouse_surname', '$spouse_firstname', '$spouse_middlename', '$spouse_extensionname',
-        '$spouse_occupation', '$spouse_employer', '$spouse_business_address', '$spouse_telephone'
+        '$spouse_occupation', '$spouse_employer', '$spouse_business_address', '$spouse_telephone', '$father_surname', '$father_firstname', '$father_middlename', '$father_extension',
+        '$mother_surname', '$mother_firstname', '$mother_middlename',
+        '$elem_nameOfSchool', '$elem_degree', '$elem_from', '$elem_to', '$elem_high', '$elem_grad', '$elem_honor',
+        '$sec_nameOfSchool', '$sec_degree', '$sec_from', '$sec_to', '$sec_high', '$sec_grad', '$sec_honor',
+        '$voc_nameOfSchool', '$voc_degree', '$voc_from', '$voc_to', '$voc_high', '$voc_grad', '$voc_honor',
+        '$college_nameOfSchool', '$college_degree', '$college_from', '$college_to', '$college_high', '$college_grad', '$college_honor',
+        '$grad_nameOfSchool', '$grad_degree', '$grad_from', '$grad_to', '$grad_high', '$grad_grad', '$grad_honor'
     )";
 
     if (mysqli_query($conn, $insertEmployeeQuery)) {
