69e568fa2815da871f988b34db7913930445b60d
Zyprush <alberiojake27@gmail.com>
2023-12-07 14:37:40 +0800

    sss

--- includes/log-header.php
+++ includes/log-header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="../assets/css/log.css" />
-    <title>Sign in & Sign up Form</title>
+    <title>Sign in</title>
     <link rel="shortcut icon" type="image/x-icon" href="../assets/img/icons.jpg">
 </head>
 
